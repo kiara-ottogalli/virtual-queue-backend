b47d42d73fe830eeb62eb6a9b2a3d8140c1ab31c
Kiara Ottogalli <kiara.ottogalli@gmail.com>
2016-06-03 18:24:49 -0400

    Added AppRole model with base model Role,  with the idea to expose the API and put proper acls. Changed init boot script.

--- server/boot/init-users-and-roles.js
+++ server/boot/init-users-and-roles.js
@@ -8,66 +8,69 @@ module.exports = function(app) {
    */
   var Mongo = app.dataSources.MongoDB;
   var User = app.models.AppUser;
-  var Role = app.models.Role;
+  var Role = app.models.AppRole;
   var RoleMapping = app.models.RoleMapping;
-  
-  Role.findOrCreate({
-	where: {name: 'admin'}
-  },
-  {
-	name: 'admin'
-  },
-  function(err, role, created){
-	if(err) throw(err);
-	
-	if(created)
-	  console.log('Created role:', role.name);
-    else
-      console.log('Found role:', role.name);
+
+  Mongo.autoupdate(function (err, result) {
     
-	User.findOrCreate({
-	  where: {username: 'admin'}
+    Role.findOrCreate({
+      where: {name: 'admin'}
     },
     {
-	  username: 'admin',
-	  email: 'kiara.ottogalli@gmail.com',
-	  password: 'admin',
-	  name: 'Kiara'
+      name: 'admin'
     },
-    function(err, user, created){
-	  if(err) throw(err);
-	
-	  if(created){
-		console.log('Created admin user:', user.username);
-		
-		role.principals.create({
-  	      principalType: RoleMapping.USER,
-  		  principalId: user.id
-	    },
-	    function(err, principal){
-		  if(err) throw(err);
-		  
-		  console.log('User: ', user.username, ' and Role: ', role.name, ' related');
-		  
-	    });
-	  }
-	  else
-		console.log('Found admin user:', user.username);
-	});
+    function(err, role, created){
+      if(err) throw(err);
+    
+      if(created)
+        console.log('Created role:', role.name);
+      else
+        console.log('Found role:', role.name);
+      
+      User.findOrCreate({
+        where: {username: 'admin'}
+      },
+      {
+        username: 'admin',
+        email: 'kiara.ottogalli@gmail.com',
+        password: 'admin',
+        name: 'Kiara'
+      },
+      function(err, user, created){
+        if(err) throw(err);
+    
+        if(created){
+          console.log('Created admin user:', user.username);
+      
+          role.principals.create({
+            principalType: RoleMapping.USER,
+            principalId: user.id
+          },
+          function(err, principal){
+            if(err) throw(err);
+            console.log('User: ', user.username, ' and Role: ', role.name, ' related');
+          });
+        }
+        else
+          console.log('Found admin user:', user.username);
+      });
+    });
+    
+    Role.findOrCreate({
+      where: {name: 'doctor'}
+    },
+    {
+      name: 'doctor'
+    },
+    function(err, role, created){
+      if(err) throw(err);
+    
+      if(created)
+        console.log('Created role:', role.name);
+      else
+        console.log('Found role:', role.name);
+    });
+  
   });
   
-  Role.findOrCreate({
-	where: {name: 'doctor'}
-  },
-  {
-	name: 'doctor'
-  },
-  function(err, role, created){
-	if(err) throw(err);
-	
-	if(created)
-	  console.log('Created role:', role.name);
-    else
-      console.log('Found role:', role.name);
-  });
 }
