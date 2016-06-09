39fa122e4b93301d7393240609d258f864190339
Kiara Ottogalli <kiara.ottogalli@gmail.com>
2016-06-09 12:13:54 -0400

    Changed init boot script to add doctors and specialties.

--- server/boot/init-users-and-roles.js
+++ server/boot/init-users-and-roles.js
@@ -22,20 +22,21 @@ module.exports = function(app) {
       {name: 'dermatology', image: 'dermatology.jpg'},
       {name: 'gynecology', image: 'gynecology.jpg'}
     ];
-    
-    specialties.forEach(function(element) {
-      Specialty.findOrCreate({
-        where: {name: element.name}
-      },
-      element,
-      function (err, specialty, created) {
-        if(err) throw(err);
-        if(created)
-          console.log('Created specialty: ', specialty.name);
-        else
-          console.log('Found specialty: ', specialty.name);
-      });
-    });
+
+    var doctors = [
+      {name: 'Daniel', lastname: 'Rosquete', phone: '+58 1234567', username: 'daniel', password: '1234', email: 'daniel@email.com'},
+      {name: 'Amadís', lastname: 'Martínez', phone: '+58 1234567', username: 'amadis', password: '1234', email: 'amadis@email.com'},
+      {name: 'Julia', lastname: 'Jackson', phone: '+58 1234567', username: 'julia', password: '1234', email: 'julia@email.com'},
+      {name: 'Helen', lastname: 'Moritz', phone: '+58 1234567', username: 'helen', password: '1234', email: 'helen@email.com'},
+      {name: 'Maria', lastname: 'Mena', phone: '+58 1234567', username: 'maria', password: '1234', email: 'maria@email.com'},
+      {name: 'Luis', lastname: 'León', phone: '+58 1234567', username: 'luis', password: '1234', email: 'luis@email.com'},
+      {name: 'Rosmary', lastname: 'Ferdinand', phone: '+58 1234567', username: 'rosmary', password: '1234', email: 'rosmary@email.com'},
+      {name: 'Daryl', lastname: 'Cross', phone: '+58 1234567', username: 'daryl', password: '1234', email: 'daryl@email.com'},
+      {name: 'Jorge', lastname: 'Negrete', phone: '+58 1234567', username: 'jorge', password: '1234', email: 'jorge@email.com'},
+      {name: 'Margery', lastname: 'Campbell', phone: '+58 1234567', username: 'margery', password: '1234', email: 'margery@email.com'},
+      {name: 'Jasmine', lastname: 'Grey', phone: '+58 1234567', username: 'jasmine', password: '1234', email: 'jasmine@email.com'},
+      {name: 'Clara', lastname: 'Gómez', phone: '+58 1234567', username: 'clara', password: '1234', email: 'clara@email.com'}
+    ];
     
     Role.findOrCreate({
       where: {name: 'admin'}
@@ -89,8 +90,53 @@ module.exports = function(app) {
     function(err, role, created){
       if(err) throw(err);
     
-      if(created)
+      if(created){
         console.log('Created role:', role.name);
+        specialties.forEach(function(element, index) {
+          Specialty.findOrCreate({
+            where: {name: element.name}
+          },
+          element,
+          function (err, specialty, created) {
+            if(err) throw(err);
+            if(created){
+              console.log('Created specialty: ', specialty.name);
+              specialty.doctors.create(
+                doctors[index],
+                function (err, doctor) {
+                  if(err) throw(err);
+                  console.log('Created doctor: ', doctor.username);
+                  role.roleMappings.create({
+                    principalType: RoleMapping.USER,
+                    principalId: doctor.id
+                  },
+                  function(err, principal){
+                    if(err) throw(err);
+                    console.log('Doctor: ', doctor.username, ' and Role: ', role.name, ' related');
+                  });
+                }
+              );
+              specialty.doctors.create(
+                doctors[index+6],
+                function (err, doctor) {
+                  if(err) throw(err);
+                  console.log('Created doctor: ', doctor.username);
+                  role.roleMappings.create({
+                    principalType: RoleMapping.USER,
+                    principalId: doctor.id
+                  },
+                  function(err, principal){
+                    if(err) throw(err);
+                    console.log('Doctor: ', doctor.username, ' and Role: ', role.name, ' related');
+                  });
+                }
+              );
+            }
+            else
+              console.log('Found specialty: ', specialty.name);
+          });
+        });
+      }
       else
         console.log('Found role:', role.name);
     });
